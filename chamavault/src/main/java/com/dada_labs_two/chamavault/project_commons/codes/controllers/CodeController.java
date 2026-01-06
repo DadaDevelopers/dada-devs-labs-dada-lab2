106007bae78aae39b1059a04c616f864f16c4c99
nellynakhero <nelly.nakhero@gmail.com>
2026-01-06 23:20:43 +0300

    added code impl service

---
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/project_commons/codes/controllers/CodeController.java
@@ -0,0 +1,60 @@
+package com.dada_labs_two.chamavault.project_commons.codes.controllers;
+
+import com.dada_labs_two.chamavault.project_commons.codes.dtos.CodeDTO;
+import com.dada_labs_two.chamavault.project_commons.codes.dtos.ValidateCodeDTO;
+import com.dada_labs_two.chamavault.project_commons.codes.models.Code;
+import com.dada_labs_two.chamavault.project_commons.codes.services.CodeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/codes")
+@RequiredArgsConstructor
+public class CodeController {
+    private static  final String  CODE = "/{code}";
+
+    private final CodeService codesService;
+
+    @PostMapping
+    public ResponseEntity<Code> addCode(@RequestBody @Valid CodeDTO codesDTO){
+        return new ResponseEntity<>(codesService.createCode(codesDTO), HttpStatus.OK);
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity<Code> validateCode(@RequestBody @Valid ValidateCodeDTO codesDTO){
+        return new ResponseEntity<>(codesService.validateCode(codesDTO), HttpStatus.OK);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyCode(@RequestBody @Valid ValidateCodeDTO codesDTO){
+        //validate
+        codesService.validateCode(codesDTO);
+        //then delete
+        codesService.deleteByCode(codesDTO.getCode());
+
+        return new ResponseEntity<>("code successfully verified", HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = CODE)
+    public ResponseEntity<String> deleteCode(@PathVariable String code){
+        codesService.deleteByCode(code);
+        return new ResponseEntity<>("delete code", HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Code>> getAllCodes(Pageable pageable){
+        return new ResponseEntity<>(codesService.listCodes(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(value = CODE)
+    public ResponseEntity<Optional<Code>> getCode(@PathVariable String code){
+        return new ResponseEntity<>(codesService.findByCode(code), HttpStatus.OK);
+    }
+}
---
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/project_commons/codes/services/CodeService.java
@@ -0,0 +1,73 @@
+package com.dada_labs_two.chamavault.project_commons.codes.services;
+
+import com.dada_labs_two.chamavault.project_commons.codes.dtos.CodeDTO;
+import com.dada_labs_two.chamavault.project_commons.codes.dtos.ValidateCodeDTO;
+import com.dada_labs_two.chamavault.project_commons.codes.models.Code;
+import com.dada_labs_two.chamavault.project_commons.codes.repository.CodeRepository;
+import com.dada_labs_two.chamavault.users.models.User;
+import com.dada_labs_two.chamavault.users.repository.UserRepository;
+import io.micrometer.common.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static com.dada_labs_two.chamavault.users.services.UserService.randomCharGenerator;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CodeService {
+    private final CodeRepository codeRepository;
+    private final UserRepository userRepository;
+
+    public Code createCode(CodeDTO codesDTO) {
+        //check owner exists
+        User user = userRepository.findByMsisdn(codesDTO.getOwnerMsisdn()).orElseThrow(()-> new RuntimeException("User not found"));
+
+        //check if code got passed, if not generate
+        String code = StringUtils.isBlank(codesDTO.getCode()) ?
+                randomCharGenerator().toUpperCase() : codesDTO.getCode();
+
+        //check expiration, default is 15 mins
+        ZonedDateTime expiryDate = codesDTO.getExpirationDate() == null ?ZonedDateTime.now().plusMinutes(15) : codesDTO.getExpirationDate();
+
+        return codeRepository.save(Code.builder()
+                .code(code)
+                .name(codesDTO.getName())
+                .active(codesDTO.isActive())
+                .description(codesDTO.getDescription())
+                .expirationDate(expiryDate)
+                .owner(user)
+                .extraData(codesDTO.getExtraData())
+                .build());
+    }
+
+    public Code validateCode(ValidateCodeDTO validateCodeDTO) {
+        //check code by the owner exist and is active
+        User owner = userRepository.findByMsisdn(validateCodeDTO.getOwnerMsisdn())
+                .orElseThrow(()-> new RuntimeException("User not found"));
+        Code code = codeRepository.findByCodeAndOwnerAndActiveTrue(validateCodeDTO.getCode(), owner).orElseThrow(
+                ()-> new RuntimeException("No active code was found for user with phone: " + validateCodeDTO.getOwnerMsisdn()));
+        //check if expired
+        if (code.getExpirationDate().isBefore(ZonedDateTime.now()))
+            throw new RuntimeException("Code has expired");
+        return code;
+    }
+
+    public Optional<Code> findByCode(String code) {
+        return codeRepository.findById(code);
+    }
+
+    public void  deleteByCode(String code) {
+        codeRepository.deleteById(code);
+    }
+
+    public Page<Code> listCodes(Pageable pageable) {
+        return codeRepository.findAll(pageable);
+    }
+}
