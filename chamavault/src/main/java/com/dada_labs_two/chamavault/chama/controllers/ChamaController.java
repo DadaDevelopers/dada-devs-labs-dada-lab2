9388e616a11847907ef5042945bd2d84074caba1
nellynakhero <nelly.nakhero@gmail.com>
2026-01-13 13:56:13 +0300

    adds list chamas request and approval functionality

--- chamavault/src/main/java/com/dada_labs_two/chamavault/chama/controllers/ChamaController.java
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/chama/controllers/ChamaController.java
@@ -1,14 +1,23 @@
 package com.dada_labs_two.chamavault.chama.controllers;
 
+import com.dada_labs_two.chamavault.chama.constants.ChamaRole;
 import com.dada_labs_two.chamavault.chama.constants.ChamaVisibility;
+import com.dada_labs_two.chamavault.chama.constants.MembershipStatus;
+import com.dada_labs_two.chamavault.chama.dtos.ChamaInviteDTO;
 import com.dada_labs_two.chamavault.chama.dtos.CreateChamaDTO;
+import com.dada_labs_two.chamavault.chama.dtos.CreateChamaInviteDTO;
+import com.dada_labs_two.chamavault.chama.dtos.JoinChamaByInviteCodeDTO;
 import com.dada_labs_two.chamavault.chama.models.Chama;
+import com.dada_labs_two.chamavault.chama.models.ChamaInvite;
+import com.dada_labs_two.chamavault.chama.models.ChamaMember;
 import com.dada_labs_two.chamavault.chama.services.ChamaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/chama")
@@ -24,5 +33,66 @@ public class ChamaController {
     @GetMapping
     public ResponseEntity<Page<Chama>> getAllChamas(Pageable pageable, @RequestParam(required = false) ChamaVisibility visibility) {
         return ResponseEntity.ok(chamaService.getChamas(pageable, visibility));
+    }
+
+    @PostMapping("/create/invite")
+    public ResponseEntity<ChamaInviteDTO>  createChamaInvite(@RequestBody CreateChamaInviteDTO invite) {
+        return ResponseEntity.ok(toChamaInviteDTO(chamaService.generateChamaInvite(invite)));
+    }
+
+    @PutMapping("/pause/invites/{inviteCode}")
+    public ResponseEntity<String> pauseChamaInvite(@PathVariable String inviteCode) {
+        chamaService.pauseInvites(inviteCode);
+        return ResponseEntity.ok("status for the invite successfully paused");
+    }
+
+    @GetMapping("/fetch/invites/{chamaReferenceId}")
+    public ResponseEntity<Page<ChamaInviteDTO>> fetchChamaInviteCodes(Pageable page,@PathVariable UUID chamaReferenceId) {
+        Page<ChamaInviteDTO> chamaInviteDTOS = chamaService.fetchChamaInviteCodes(page, chamaReferenceId)
+                .map(this::toChamaInviteDTO);
+
+        return ResponseEntity.ok(chamaInviteDTOS);
+    }
+
+    @PostMapping("/join/invite")
+    public ResponseEntity<ChamaMember> joinChamaByInviteCode(@RequestBody JoinChamaByInviteCodeDTO joinChama) {
+        return ResponseEntity.ok(chamaService.joinChamaByInviteCode(joinChama));
+    }
+
+    @PostMapping("/join/{chamaId}/request")
+    public ResponseEntity<ChamaMember> requestToJoinChama(@PathVariable UUID chamaId,
+                                                          @RequestParam ChamaRole role,
+                                                          @RequestParam String joinerPhone) {
+        return ResponseEntity.ok(chamaService.requestToJoinChama(chamaId, role, joinerPhone));
+    }
+
+    @GetMapping("/members-by-status")
+    public ResponseEntity<Page<ChamaMember>> getChamaMembersByStatus(Pageable pageable,
+                                                                     @RequestParam MembershipStatus status,
+                                                                     @RequestParam UUID chamaReferenceId) {
+        return ResponseEntity.ok(chamaService.fetchChamaMembersByStatus(pageable, chamaReferenceId, status));
+    }
+
+    @PostMapping("/{chamaId}/members/{memberId}/approve")
+    public ResponseEntity<ChamaMember> approveUserRequestToJoinChama(@PathVariable UUID chamaId,
+                                                                     @RequestParam String approverPhone,
+                                                                     @PathVariable UUID memberId,
+                                                                     @RequestParam MembershipStatus status) {
+        return ResponseEntity.ok(chamaService.approveUserRequestToJoinChama(chamaId, approverPhone, memberId, status));
+    }
+
+
+    ChamaInviteDTO toChamaInviteDTO(ChamaInvite chamaInvite) {
+        return ChamaInviteDTO.builder()
+                .inviteReference(chamaInvite.getInviteReference())
+                .chama(chamaInvite.getChama())
+                .inviteCode(chamaInvite.getInviteCode().getCode())
+                .role(chamaInvite.getRole())
+                .requiresApproval(chamaInvite.getRequiresApproval())
+                .used(chamaInvite.getUsed())
+                .paused(chamaInvite.getPaused())
+                .createdAt(chamaInvite.getCreatedAt())
+                .expiresAt(chamaInvite.getExpiresAt())
+                .build();
     }
 }
--- chamavault/src/main/java/com/dada_labs_two/chamavault/chama/models/ChamaInvite.java
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/chama/models/ChamaInvite.java
@@ -1,8 +1,7 @@
 package com.dada_labs_two.chamavault.chama.models;
 
 import com.dada_labs_two.chamavault.chama.constants.ChamaRole;
-import com.dada_labs_two.chamavault.chama.constants.MembershipStatus;
-import com.dada_labs_two.chamavault.users.models.User;
+import com.dada_labs_two.chamavault.project_commons.codes.models.Code;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,40 +12,50 @@ import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "chama_members",
-        indexes = {
-        @Index(name = "idx_chama_member_chama", columnList = "chama_reference"),
-        @Index(name = "idx_chama_member_status", columnList = "status"),
-})
+@Table(name = "chama_invites")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @SQLRestriction("deleted_at is null")
-public class ChamaMember {
+public class ChamaInvite {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID reference;
+    private UUID inviteReference;
 
     @ManyToOne
     @JoinColumn(name = "chama_reference")
     private Chama chama;
 
     @ManyToOne
-    @JoinColumn(name = "user_reference")
-    private User user;
+    @JoinColumn(name = "code")
+    private Code inviteCode;
+
+    @OneToMany
+    @JoinColumn(name = "chama_member_reference")
+    private List<ChamaMember> chamaMembers = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
-    private ChamaRole role;
+    private ChamaRole role; // Role this invite grants
 
-    @Enumerated(EnumType.STRING)
-    private MembershipStatus status; // ACTIVE / PENDING / REJECTED
+    @Column(nullable = false)
+    private Boolean requiresApproval;
+
+    @Column(nullable = false)
+    private Boolean used = false;
+
+    @Column(nullable = false)
+    private Boolean paused = false;
+
+    private ZonedDateTime expiresAt;
 
     @CreationTimestamp
-    private ZonedDateTime joinedAt;
+    private ZonedDateTime createdAt;
 
     @UpdateTimestamp
     private ZonedDateTime updatedAt;
