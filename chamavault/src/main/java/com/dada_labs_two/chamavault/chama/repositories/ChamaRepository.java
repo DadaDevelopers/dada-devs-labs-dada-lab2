ff32079af09a4d7fa59ac460fd60e91c4738a751
nellynakhero <nelly.nakhero@gmail.com>
2026-01-13 08:54:28 +0300

    adds list chamas functionality

--- chamavault/src/main/java/com/dada_labs_two/chamavault/chama/repositories/ChamaRepository.java
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/chama/repositories/ChamaRepository.java
@@ -1,9 +1,13 @@
 package com.dada_labs_two.chamavault.chama.repositories;
 
+import com.dada_labs_two.chamavault.chama.constants.ChamaVisibility;
 import com.dada_labs_two.chamavault.chama.models.Chama;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ChamaRepository extends JpaRepository<Chama, UUID> {
+    Page<Chama> findAllByVisibility(Pageable pageable, ChamaVisibility visibility);
 }
