b3d899d8d83447908206dd70a96b74d369018021
mercie-ux <mbaomercy0@gmail.com>
2026-01-15 09:39:19 +0300

    fix: cta login link

--- lightning-chama/src/app/landing-page/HeroSection.tsx
+++ lightning-chama/src/app/landing-page/HeroSection.tsx
@@ -35,7 +35,7 @@ export function HeroSection() {
               </button>
             </Link>
             {/*note: to change the /userdashboard with the correct link /landing-page/login */}
-            <Link href="/userdashboard" >
+            <Link href="/landing-page/login" >
               <button className="px-8 py-4 bg-white text-[#059669] border-2 border-[#059669] rounded-2xl text-2xl font-semibold cursor-pointer hover:bg-emerald-50 transition-colors">
                 Login
               </button>
