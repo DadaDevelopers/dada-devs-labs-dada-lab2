75c26d24dad2f591a2f5bc1d21eebc839e516b88
nellynakhero <nelly.nakhero@gmail.com>
2026-02-01 20:13:11 +0300

    updates

--- chamavault/src/main/java/com/dada_labs_two/chamavault/wallets/dtos/InvoiceDto.java
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/wallets/dtos/InvoiceDto.java
@@ -1,5 +1,7 @@
 package com.dada_labs_two.chamavault.wallets.dtos;
 
+import com.dada_labs_two.chamavault.wallets.constants.InvoiceStatus;
+
 import java.time.ZonedDateTime;
 
 public record InvoiceDto(
@@ -9,6 +11,7 @@ public record InvoiceDto(
         long amountMsats,
         long amountFees,
         String qrCode,
+        InvoiceStatus status,
         ZonedDateTime expiresAt
 ) {}
 
