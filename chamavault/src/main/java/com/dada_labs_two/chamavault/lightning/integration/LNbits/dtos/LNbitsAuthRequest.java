282359cef6f6bc2ade51d505682962e4fbf17552
nellynakhero <nelly.nakhero@gmail.com>
2026-09-07 18:05:59 +0300

    add use of bearer token on lnbits APIs

---
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/lightning/integration/LNbits/dtos/LNbitsAuthRequest.java
@@ -0,0 +1,4 @@
+package com.dada_labs_two.chamavault.lightning.integration.LNbits.dtos;
+
+public record LNbitsAuthRequest(String username, String password) {
+}
---
+++ chamavault/src/main/java/com/dada_labs_two/chamavault/lightning/integration/LNbits/dtos/LNbitsAuthResponse.java
@@ -0,0 +1,4 @@
+package com.dada_labs_two.chamavault.lightning.integration.LNbits.dtos;
+
+public record LNbitsAuthResponse(String access_token, String token_type) {
+}
